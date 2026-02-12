6da77cc059f8d49c66a0d3e6863ed4e4b803eaae
vidisha07 <102941860+vidisha07@users.noreply.github.com>
2026-02-12 10:58:53 -0500

    Add manifest for sbm_hr_core

---
+++ addons/sbm_hr_core/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "SBM HR Core",
+    "version": "19.0.1.0.0",
+    "category": "Human Resources",
+    "summary": "SBM core HR configuration, locations, employment types, and language preference.",
+    "depends": ["base", "hr"],
+    "data": [
+        "views/menu.xml",
+    ],
+    "installable": True,
+    "application": False,
+    "license": "LGPL-3",
+}
