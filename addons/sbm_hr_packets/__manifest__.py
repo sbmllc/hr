169312367dbee92ac55f846d08218e2a2d84f6ee
vidisha07 <102941860+vidisha07@users.noreply.github.com>
2026-02-12 11:40:46 -0500

    Add manifest for sbm_hr_packets

---
+++ addons/sbm_hr_packets/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "SBM HR Packets",
+    "version": "19.0.1.0.0",
+    "category": "Human Resources",
+    "summary": "Automated onboarding/offboarding packet assignment and e-sign workflow.",
+    "depends": ["base", "hr", "sign"],
+    "data": [
+        "views/menu.xml",
+    ],
+    "installable": True,
+    "application": False,
+    "license": "LGPL-3",
+}
